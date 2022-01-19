bb4ee4040cbd68b3620fe0f3da54115e5464a1c7
tetsuoYoshikawa <bball.t3y18@gmail.com>
2022-01-19 15:19:28 +0900

    heroku-update9

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
             'email' => 'admin@admin.com',
             'email_verified_at' => now(),
             'password' => Hash::make('adminadmin'),
-            'avatar_file_name' => 'UElz5lK1BFeskcmAq2mdLNS0DgpxJsKoonWsD4du.png',
+            'avatar_file_name' => '90BmqcOCPjfFLZtJz9cCSIwq9Snsv8VWrI4fo99i.png',
             'administrator' => true,
             'content' => 'ヨーロッパやアメリカ、ドメスティックと国内外を問わずメンズ・レディース共に幅広くセレクトしています。NEW・USEDウエアー/アクセサリー/服飾・生活雑貨/インテリアなどなど数多く取り揃えております。気軽にお問い合わせください。',
             'url' => 'http://junkshop.chronicle.co.jp/'
